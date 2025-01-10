ff30f2ff0bca20fef74c7ff69acc51a2c5274c21
asher <asherstewart07@gmail.com>
2025-01-09 18:52:37 -0600

    icg
    lord

---
+++ mosis-website/mosis-website/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
